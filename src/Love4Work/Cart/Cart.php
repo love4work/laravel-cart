1959d583a2ea1121ecf9c4b43be4a7f2e03e5c39
Peter Quentin <peter@love4work.com>
2016-07-21 16:46:50 +0200

    Initial commit, extend default classes for now. lazy

---
+++ src/Love4Work/Cart/Cart.php
@@ -0,0 +1,13 @@
+<?php namespace Love4Work\Cart;
+
+use Darryldecode\Cart\Cart as ThirdPartyCart;
+
+/**
+ * Class Cart
+ * @package Love4Work\Cart
+ */
+class Cart extends ThirdPartyCart {
+
+
+
+}
---
+++ src/Love4Work/Cart/CartCondition.php
@@ -0,0 +1,13 @@
+<?php namespace Love4Work\Cart;
+
+use Darryldecode\Cart\Cart as ThirdPartyCartCondition;
+
+/**
+ * Class CartCondition
+ * @package Love4Work\Cart
+ */
+class CartCondition extends ThirdPartyCartCondition {
+
+
+
+}
